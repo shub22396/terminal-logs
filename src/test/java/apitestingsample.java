afaf7b89fb575b6592ca7f9f38f67378a993fd49
shub22396 <rathore.shubham14@stu.upes.ac.in>
2022-02-22 17:28:08 +0530

    First Commit

---
+++ src/test/java/apitestingsample.java
@@ -0,0 +1,198 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import io.restassured.RestAssured.*;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import org.hamcrest.Matchers.*;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.sessionId;
+
+
+public class apitestingsample {
+
+    public String username = "shubhamr";    //lambda UserName
+    public String accesskey = "bKCQ8wHNM5QHi8m3c9vc9JecZjXeWN34SfNIEBYZywYAPoC87Z"; //lambda accessKey
+    public RemoteWebDriver driver;
+    public String gridURL = "@hub.lambdatest.com/wd/hub";
+    public String sess;
+    String status;
+    public int b;
+
+
+
+    @BeforeTest
+    @org.testng.annotations.Parameters(value = {"browser", "version", "platform", "resolution"})
+
+
+    public void setUp(String browser, String version, String platform, String resolution) throws Exception {
+
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("browserName", browser);
+        capabilities.setCapability("browserVersion", version);
+        capabilities.setCapability("platformName", platform);
+        capabilities.setCapability("resolution",resolution);
+        capabilities.setCapability("build", "API_TESTING_SAMPLE_LAMBDATEST");
+        capabilities.setCapability("console",true);
+        //capabilities.setCapability("name","TestID12345");
+
+
+        try {
+            String url = "https://" + username + ":" + accesskey + gridURL;
+
+            driver = new RemoteWebDriver(new URL(url), capabilities);
+
+
+        } catch (MalformedURLException e) {
+            System.out.println("Invalid grid URL");
+        } catch (Exception f) {
+            System.out.println(f.getMessage());
+        }
+    }
+
+
+
+
+
+    @Test(priority = 1)
+    @org.testng.annotations.Parameters(value = {"platform", "browser", "version", "resolution"})
+
+
+    public void sample() {
+        String session="output"+driver.getSessionId();
+        //System.out.print(session);
+
+
+
+        try {
+            //to save the console output on to a txt file
+
+           System.setOut(new PrintStream(new FileOutputStream("/home/ubuntu/workspace/Internet Store Terminal Logs/"+session+".txt")));
+//
+//            File dir = new File("D:\\API_Sample-master\\output");
+//            if(!dir.exists()) {
+//                dir.mkdirs();
+//            }
+////            FileWriter fstream = new FileWriter(dir.getAbsolutePath() + "\\" + driver.getSessionId()+".txt", true);
+//            System.setOut(new PrintStream(new FileOutputStream(dir.getAbsolutePath() + "\\" + driver.getSessionId()+".txt", true)));
+
+            // Launch the app
+            driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+            // Click on First Item
+            driver.findElement(By.name("li")).click();
+
+            // Click on Second Item
+            driver.findElement(By.name("li2")).click();
+
+            // Add new item is list
+            driver.findElement(By.id("sampletodotext")).clear();
+            driver.findElement(By.id("sampletodotext")).sendKeys("Yey, Let's add it to list");
+            driver.findElement(By.id("addbutton")).click();
+
+            // Verify Added item
+            String item = driver.findElement(By.xpath("/html/body/div/div/div/ul/li[6]/span")).getText();
+            status = "passed";
+            b=b+1;
+
+           // System.out.print("b---------------->"+b);
+
+        }
+        catch (Exception e) {
+            status = "failed";
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+
+
+
+//    @Test(priority = 2)
+//    public void uploadFile() {           //uploading the logs to the terminal logs tab on lambdatest
+//
+//
+//            File testUploadFile = new File("D:\\API_Sample-master\\output"+driver.getSessionId()+"op.txt"); //Specify your own location and file
+//
+//            RestAssured.baseURI = "https://api.lambdatest.com/automation/api/v1/sessions/"+driver.getSessionId()+"/terminal-logs";
+//
+//            String Name="out-"+driver.getSessionId()+".txt";
+//            System.out.print("Name------>"+Name+"  -    ");
+//            String current=Name.substring(3,40);
+//            String x="-"+driver.getSessionId();
+//            System.out.print("Current----->"+current+"-"+x+"  ");
+//             int y = 0;
+//
+//
+//
+//
+//
+////              if(Name!=null)
+////              { Response response = given().auth().basic(username,accesskey)
+////                    .multiPart(testUploadFile)
+////                    .when().post();
+////
+////                  System.out.println(response.getStatusCode());
+////                  System.out.println(response.asString());
+////                  y=y+1;
+////                System.out.print("flag="+y+"");
+////
+////                 Name=null;
+////
+////                  //System.out.print("---1--"+Name+"");
+////              }
+//
+//
+//                  Response response = given().auth().basic(username,accesskey)
+//                    .multiPart(testUploadFile)
+//                    .when().post();
+//
+//                  System.out.println(response.getStatusCode());
+//                  System.out.println(response.asString());
+//                  y=y+1;
+//                System.out.print("flag="+y+"");
+//
+//
+//
+//
+////              else
+////              {
+////                  System.out.print("invalid session ID");
+////                  //Name="output"+driver.getSessionId()+"op.txt";
+////              }
+//
+//        }
+
+
+    @AfterTest
+    @org.testng.annotations.Parameters(value = {"platform", "browser", "version", "resolution"})
+    public void tearDown(String platform, String browser, String version, String resolution) throws Exception {
+
+        if (driver != null) {
+            ((JavascriptExecutor) driver).executeScript("lambda-status=" + status);
+
+           // driver.quit();
+        }
+    }
+
+
+
+}
+
---
+++ src/test/java/uploadfile.java
@@ -0,0 +1,156 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import io.restassured.RestAssured.*;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import org.hamcrest.Matchers.*;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.sessionId;
+
+
+public class uploadfile {
+    public String username = "shubhamr";    //lambda UserName
+    public String accesskey = "bKCQ8wHNM5QHi8m3c9vc9JecZjXeWN34SfNIEBYZywYAPoC87Z"; //lambda accessKey
+    public RemoteWebDriver driver;
+    public String gridURL = "@hub.lambdatest.com/wd/hub";
+    String status;
+    String fileName;
+    String gName;
+
+
+
+
+
+
+
+    @Test(priority = 2)
+    public void uploadFile() {           //uploading the logs to the terminal logs tab on lambdatest
+     int n=0;
+
+
+
+
+    File folder = new File("/home/ubuntu/workspace/Internet Store Terminal Logs");
+    File[] listOfFiles = folder.listFiles();
+
+    for (int i = 0; i < listOfFiles.length; i++) {
+
+        if (listOfFiles[i].isFile()) {
+            String fileName = listOfFiles[i].getName();
+            if (fileName.endsWith(".txt")) {
+               // System.out.println("File ---------------------------->" + fileName);
+                String gName = fileName;
+                gName= gName.replaceAll("[-]","");
+                System.out.print("replacegname:"+gName);
+               gName = gName.substring(6, 38);
+
+               System.out.print("GNAME substring:------------>"+gName);
+               String cName= fileName;
+               cName=cName.replace("output","");
+               cName=cName.replace("op","");
+               String fName=cName.replace(".txt","");
+
+             System.out.print("cname===="+cName);
+              System.out.print("fname====="+fName);
+
+
+
+                File testUploadFile = new File("/home/ubuntu/workspace/Internet Store Terminal Logs/"+"output"+cName+"");
+                System.out.print(testUploadFile);
+
+                RestAssured.baseURI = "https://api.lambdatest.com/automation/api/v1/sessions/"+fName+"/terminal-logs";
+
+
+                Response response = given().auth().basic(username, accesskey)
+                        .multiPart(testUploadFile)
+                        .when().post();
+
+                System.out.println(response.getStatusCode());
+                System.out.println(response.asString());
+
+                System.out.print("---------------->"+i);
+            }
+        }
+    }
+
+
+
+
+//
+//        File testUploadFile = new File("D:\\API_Sample-master\\output"+gName+"op.txt");
+//        System.out.print(testUploadFile);
+//
+//        RestAssured.baseURI = "https://api.lambdatest.com/automation/api/v1/sessions/"+gName+"/terminal-logs";
+//
+//
+//        Response response = given().auth().basic(username, accesskey)
+//                .multiPart(testUploadFile)
+//                .when().post();
+//
+//        System.out.println(response.getStatusCode());
+//        System.out.println(response.asString());
+
+
+
+
+
+
+
+
+        /*File testUploadFile = new File("D:\\API_Sample-master\\output"+driver.getSessionId()+"op.txt"); //Specify your own location and file
+
+        RestAssured.baseURI = "https://api.lambdatest.com/automation/api/v1/sessions/"+"8077cef009ee201018bf1463158df76"+"/terminal-logs";
+
+
+        Response response = given().auth().basic(username, accesskey)
+                .multiPart(testUploadFile)
+                .when().post();
+
+        System.out.println(response.getStatusCode());
+        System.out.println(response.asString());*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    @AfterTest
+    @org.testng.annotations.Parameters(value = {"platform", "browser", "version", "resolution"})
+    public void tearDown(String platform, String browser, String version, String resolution) throws Exception {
+
+        if (driver != null) {
+            ((JavascriptExecutor) driver).executeScript("lambda-status=" + status);
+            driver.quit();
+        }
+    }
+
+
+
+}
